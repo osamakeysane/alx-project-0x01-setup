69bc03e1f60894d6b8c624f9fdf1948d9ac95e8b
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-17 10:54:11 +0300

    .

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -30,28 +30,29 @@ interface UsersPageProps {
 
 const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="p-4">
-        <h1 className="text-2xl font-semibold mb-4">Users</h1>
+        <h1 className="text-2xl font-semibold mb-4">Users List</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {users.map((user) => (
-            <UserCard
-              key={user.id}
-              id={user.id}
-              name={user.name}
-              username={user.username}
-              email={user.email}
-              address={user.address}
-              phone={user.phone}
-              website={user.website}
-              company={user.company}
-            />
+          {users?.map((user) => (
+            <UserCard key={user.id} {...user} />
           ))}
         </div>
       </main>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
 
 export default Users;
