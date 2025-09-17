904df64f12b340002d6d31de98ac40d762a49a5a
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-17 09:33:44 +0300

    advanced

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,7 +1,20 @@
+import Button from "../components/common/Button";
+import PostCard from "../components/common/PostCard";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+
 const Home: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Welcome Page</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+
+      <main className="flex-1 flex flex-col items-center justify-center space-y-6">
+        <h1 className="text-5xl font-bold">Welcome Page</h1>
+        <PostCard />
+        <Button />
+      </main>
+
+      <Footer />
     </div>
   );
 };
