720c0b6b579ba240d5d8d72ec8cd1bfb162c0f07
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-17 09:18:15 +0300

    added some minimal contentn

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostCard: React.FC = () => {
+  return (
+    <div className="border p-4 rounded shadow">
+      <h2 className="text-xl font-semibold">Post Title</h2>
+      <p className="text-gray-600">Post content goes here...</p>
+    </div>
+  );
+};
+
+export default PostCard;
