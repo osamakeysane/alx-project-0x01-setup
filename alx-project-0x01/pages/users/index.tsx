485a090c5d29864663872282453eed03bc0e345e
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-18 15:27:09 +0300

    .

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -57,64 +57,35 @@
 // export default Users;
 
 // .....................................task 5 ........................
-import Header from "@/components/layout/Header";
+import React from "react";
 import UserCard from "@/components/common/UserCard";
-import UserModal from "@/components/common/UserModal";
-import { UserProps, UserData } from "@/interfaces";
-import { useState } from "react";
+import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-  users: UserProps[];
+  posts: UserProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ users }) => {
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [allUsers, setAllUsers] = useState<UserProps[]>(users);
-  const [user, setUser] = useState<UserData | null>(null);
-
-  const handleAddUser = (newUser: UserData) => {
-    setUser(newUser);
-    const userWithId: UserProps = { ...newUser, id: allUsers.length + 1 };
-    setAllUsers([userWithId, ...allUsers]);
-  };
-
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="p-4">
-        <div className="flex justify-between items-center mb-4">
-          <h1 className="text-2xl font-semibold">Users List</h1>
-          <button
-            onClick={() => setModalOpen(true)}
-            className="bg-blue-700 px-4 py-2 rounded-full text-white"
-          >
-            Add User
-          </button>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {allUsers?.map((user) => (
-            <UserCard key={user.id} {...user} />
-          ))}
-        </div>
-      </main>
-
-      {isModalOpen && (
-        <UserModal
-          onClose={() => setModalOpen(false)}
-          onSubmit={handleAddUser}
-        />
-      )}
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Our Users</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {posts.map((user) => (
+          <UserCard key={user.id} {...user} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const posts = await response.json();
 
   return {
-    props: { users },
+    props: {
+      posts,
+    },
   };
 }
 
